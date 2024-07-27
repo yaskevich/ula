2553cd2ea173d41b79be100a8fa8cd7900e24432
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-07-27 22:23:53 +0200

    switch objects by buttons

--- ui/src/components/D3Map.vue
+++ ui/src/components/D3Map.vue
@@ -1,4 +1,10 @@
 <template>
+  <h3>{{ streetObject?.name }} [{{ streetObject?.freq }}]</h3>
+  <ButtonGroup>
+    <Button @click="loadPrevious" :disabled="!curIndex" icon="pi pi-arrow-left"></Button>
+    <Button @click="loadNext" icon="pi pi-arrow-right"></Button>
+  </ButtonGroup>
+
   <div ref="mapRef" id="map">
     <svg ref="svgRef" :width="svgWidth" :height="svgHeight">
       <text x="0" y="15" class="title">{{ unit }}</text>
@@ -17,7 +23,7 @@ const svgHeight = ref(0);
 
 let legendOffsetY = 0;
 const legendOffsetX = 5;
-const legendHeight = 20;
+const legendHeight = 10;
 const legendRightMargin = 50;
 let legendCellWidth = 5;
 
@@ -26,8 +32,16 @@ const num = ref('');
 const svgRef = ref(null);
 const mapRef = ref<HTMLElement | null>(null);
 
-onMounted(() => {
+const streetObject = ref<IStreetInfo>();
+const getCount = (id: number) => streetObject.value?.regions?.[id] || 0;
+
+const curIndex = ref(0);
+
+const loadStreet = () => {
+
   let svg = d3.select(svgRef.value);
+
+  svg.selectAll("*").remove();
   const parentNode = d3.select(mapRef.value).node();
 
   if (parentNode !== null) {
@@ -36,24 +50,19 @@ onMounted(() => {
 
   svgHeight.value = svgWidth.value * 1;
   // console.log(svgWidth.value, svgHeight.value);
-  legendOffsetY = svgHeight.value - 50;
+  legendOffsetY = svgHeight.value - 30;
 
   const projection = d3.geoMercator()
     .center([19.480556, 52.069167])
     .scale(svgWidth.value / Math.PI * 16)
     .translate([svgWidth.value >> 1, svgHeight.value >> 1]);
 
-  const ext = d3.extent(store.geofeatures, (d: any) => d.properties.num as number);
+  streetObject.value = Object.values(store.freq[curIndex.value])?.[0] as IStreetInfo;
 
-  if (!ext[0] || !ext[1]) {
-    return;
-  }
+  const last = d3.max(Object.values(streetObject.value.regions));
+  const colorize = d3.scaleLinear<string>().domain([0, last]).clamp(true).range(['#f1eef6', '#0570b0']);
 
-  const colorize = d3.scaleLinear<string>().domain(ext).clamp(true).range(['#f1eef6', '#0570b0']);
-  // const colorMap = d3.scaleQuantile<string>().domain(ext).range(['#f1eef6', '#0570b0']);
-  const scaleLength = ext[1] - ext[0];
-
-  legendCellWidth = (svgWidth.value - legendRightMargin) / scaleLength;
+  legendCellWidth = (svgWidth.value - legendRightMargin) / last;
 
   svg.append("g")
     .selectAll("path")
@@ -61,25 +70,12 @@ onMounted(() => {
     .join("path")
     .classed('district', true)
     .attr('fill', (d: any) => {
-      // const n = d.properties.num;
-      // if (n > 30) {
-      //   return '#0570b0';
-      // }
-      // if (n > 20) {
-      //   return '#74a9cf';
-      // }
-      // if (n > 10) {
-      //   return '#bdc9e1';
-      // }
-      // return '#f1eef6';
-      return colorize(d.properties.num);
+      return colorize(getCount(d.properties.terytId));
     })
     .attr("d", d3.geoPath().projection(projection) as any)
     .on("mouseover", (e, d: any) => {
-      console.log(d.properties);
-      
       unit.value = d.properties.nazwa + ' w-wo';
-      num.value = String(d.properties.terytId);
+      num.value = getCount(d.properties.terytId);
     })
     .on("mouseout", function () {
       unit.value = num.value = '';
@@ -89,7 +85,7 @@ onMounted(() => {
 
   legend
     .selectAll('.legend')
-    .data(d3.range(scaleLength))
+    .data(d3.range(last))
     .enter().append('rect')
     .attr('x', d => legendOffsetX + d * legendCellWidth + 'px')
     .attr("y", legendOffsetY)
@@ -100,9 +96,22 @@ onMounted(() => {
   legend
     .append("g")
     .attr("transform", `translate(${legendOffsetX},${legendOffsetY + legendHeight})`)
-    .call(d3.axisBottom(d3.scaleLinear().domain(ext).range([0, scaleLength * legendCellWidth])).ticks(Math.round(ext[1] / 10)));
+    .call(d3.axisBottom(d3.scaleLinear().domain([0, last]).range([0, last * legendCellWidth])).ticks(Math.round(last / 100)));
+};
 
-});
+const loadNext = () => {
+  curIndex.value += 1;
+  loadStreet();
+};
+
+const loadPrevious = () => {
+  if (curIndex.value) {
+    curIndex.value -= 1;
+    loadStreet();
+  }
+};
+
+onMounted(() => loadStreet());
 
 </script>
 
@@ -114,11 +123,13 @@ onMounted(() => {
     fill: #fbd0da;
   }
 }
+
 :deep(.title) {
   /* fill: #2196f3;
   fill: #0570b0; */
   fill: #5f071c;
 }
+
 :deep(.quantity) {
   /* fill: #2196f3; 
   fill: #0570b0;*/
