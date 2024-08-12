b0b950aeae62b309e9e043a0ee13dc2cacac8cdd
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-08-12 19:04:59 +0200

    update types

--- ui/src/index.d.ts
+++ ui/src/index.d.ts
@@ -1,27 +1,37 @@
-  interface IBibItem {
-    title: string;
-    link: string;
-    district: string;
-    region: string;
-    meta: string;
-    type: string;
-  }
-
-  interface ICascadeChild {
-    name: string;
-    level: number;
-    parent?: boolean;
-    label: string;
-  }
-
-  interface ICascadeItem {
-    name: string;
-    level: number;
-    label: string;
-    children: Array<ICascadeChild>;
-  }
-
-  interface ICounter {
-    [key: string]: number,
+interface IBibItem {
+  title: string;
+  link: string;
+  district: string;
+  region: string;
+  meta: string;
+  type: string;
 }
-  
+
+interface ICascadeChild {
+  name: string;
+  level: number;
+  parent?: boolean;
+  label: string;
+}
+
+interface ICascadeItem {
+  name: string;
+  level: number;
+  label: string;
+  children: Array<ICascadeChild>;
+}
+
+interface ICounter {
+  [key: string]: number,
+}
+
+interface keyable {
+  [key: string]: any;
+}
+
+interface IStreetInfo {
+  name: string;
+  freq: number;
+  regions: keyable;
+}
+
