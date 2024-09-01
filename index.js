0afeb30aa6db49f9034be2370fb2ed7a7a3978bf
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-09-01 19:41:29 +0200

    process categorized data

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const __package = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
 
+let db = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'cats.json'))).filter(x => !parseInt(x.stem));
+// console.log(db);
+
+const ontology = {};
+const dict = {};
+
+db.forEach(el => {
+  const item = { 'stem': el.stem, name: el.name };
+  if (el.feature === 'wishful') {
+    item['wishful'] = true;
+  }
+  if (el.type in ontology) {
+    ontology[el.type].push(item)
+  } else {
+    ontology[el.type] = [item]
+  }
+
+  dict[el.name] = el.stem;
+  // console.log(el.stem, el.type, el.feature);
+});
+
+// console.log(ontology);
+console.log(dict);
+
 // environment variables
 const port = process.env.PORT || 8080;
 const appName = __package?.name || String(port);
@@ -25,12 +49,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(history());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/api/data', auth, async (req, res) => {
+app.get('/api/data', async (req, res) => {
   const data = {};
   res.json(data);
 });
 
-app.post('/api/record', auth, async (req, res) => {
+app.post('/api/record', async (req, res) => {
   const word = req.body.word + '\n';
   res.send('ok');
 });
