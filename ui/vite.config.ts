d42fb1f4b38552dbc89c94937d9a766f560d2e7a
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-09-03 16:23:50 +0200

    setup proxy  for API

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  plugins: [vue(),],
   server: {
-    fs: {
-      strict: false,
-    }
+    port: Number(loadEnv('', process.cwd(), 'VITE_PORT')?.['VITE_PORT']) || 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:' + loadEnv('', process.cwd(), 'VITE_API')?.['VITE_API'],
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
   },
-  plugins: [vue(),]
 })
