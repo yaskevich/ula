5097fb8727befde5a99b143f68d1f58bb5f2e4e8
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-09-07 19:28:05 +0200

    render stems

--- ui/src/components/All.vue
+++ ui/src/components/All.vue
@@ -1,10 +1,17 @@
 <template>
     <!-- <h2>Analysis of the street names of Poland</h2> -->
+     <div v-if="isLoaded">
     <div v-for="(val, index) in store.freq.streets.slice(0, limit)">
         <Button :text="route.fullPath !== `/country/${index + 1}`" @click="router.push(`/country/${index + 1}`)">{{
         index + 1
-    }}. {{ Object.keys(val).shift() }}</Button>
+    }}. {{ val.name }}</Button>
+        <InputText v-if="showEditor === index" type="text" v-model="value" size="small" />
+        <span v-else>
+            <Chip v-if="val.name in dict" :label="dict[val.name]" />
+            <Button text icon="pi pi-pencil" @click="showEditor = index"></Button>
+        </span>
     </div>
+</div>
     <!--
       <div class="field mx-auto text-center">
           <InputText id="search" aria-describedby="search-help" type="text" v-model="userinput" @input="inputEvent" class="d-block mx-auto" />
@@ -14,14 +21,33 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onBeforeMount } from 'vue';
 import store from '../store';
-import D3Map from './D3Map.vue';
 import { useRouter, useRoute } from 'vue-router';
 
+const dict = ref();
+const showEditor = ref();
 const router = useRouter();
 const route = useRoute();
 const limit = Number(route.params.limit) || 500;
 console.log(route.params.limit);
+
+const value = ref('');
+
+const isLoaded = ref(false);
+
+onBeforeMount(async () => {
+    const response = await fetch('/api/dict');
+    if (response.status === 200) {
+        const data = await response.json();
+        // console.log(data);
+        dict.value = data;
+        isLoaded.value = true;
+    } else {
+        console.log("fetching error");
+    }
+});
+
 </script>
 
 <style scoped>
