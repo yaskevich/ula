e411f493a135762a10992e2999267d0606327233
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-09-14 16:57:57 +0200

    enable modal for app

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,31 +1,36 @@
 <template>
-  <ScrollTop />
-  <div class="container">
-    <div id="main">
-      <div id="nav">
-        <router-link to="/home">Home</router-link> |
-        <router-link to="/country/1">Country top</router-link> |
-        <router-link to="/regions/1">Regional tops</router-link>
+  <n-back-top />
+  <n-modal-provider>
+
+    <div class="container">
+      <div id="main">
+        <div id="nav">
+          <router-link to="/home">Home</router-link> |
+          <router-link to="/country/1">Country top</router-link> |
+          <router-link to="/regions/1">Regional tops</router-link> |
+          <router-link to="/list/500">Full list</router-link> |
+          <router-link to="/lexicon">Lexicon</router-link>
+        </div>
+      </div>
+      <div class="content">
+        <router-view />
+      </div>
+      <div class="footer p-2" id="about">
+        <p>
+          <a href="https://yaskevich.com/" target="_blank">Alyaxey Yaskevich</a>, 2024. Version: {{ store.version }}
+          <a :href="store.git" target="_blank">
+            <i class="pi pi-github"></i>
+          </a>
+        </p>
+        <p>
+          <small>
+            The icon «Place Marker» is from
+            <a href="https://icons8.com/icon/30622/place-marker" target="_blank">Icons8</a>.
+          </small>
+        </p>
       </div>
     </div>
-    <div class="content">
-      <router-view />
-    </div>
-    <div class="footer p-2" id="about">
-      <p>
-        <a href="https://yaskevich.com/" target="_blank">Alyaxey Yaskevich</a>, 2024. Version: {{ store.version }}
-        <a :href="store.git" target="_blank">
-          <i class="pi pi-github"></i>
-        </a>
-      </p>
-      <p>
-        <small>
-          The icon «Place Marker» is from
-          <a href="https://icons8.com/icon/30622/place-marker" target="_blank">Icons8</a>.
-        </small>
-      </p>
-    </div>
-  </div>
+  </n-modal-provider>
 </template>
 
 <script setup lang="ts">
@@ -52,6 +57,10 @@ import store from './store';
   /* background: #f8f6f6; */
   border-radius: 5px;
   padding: 5px;
+}
+
+.router-link-active {
+  font-weight: bold;
 }
 
 .content {
