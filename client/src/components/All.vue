c786cb7f60a42733d2a0c0f50b0fd92beb48deca
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-09-26 23:31:57 +0200

    fix rendering for new data structure

--- client/src/components/All.vue
+++ client/src/components/All.vue
@@ -5,12 +5,12 @@
             <n-space justify="space-between" style="max-width:300px">
                 <n-button :text="route.fullPath !== `/country/${index + 1}`"
                     @click="router.push(`/country/${index + 1}`)">{{
-        index + 1
-    }}. {{ val.name }}</n-button>
+                    index + 1
+                    }}. {{ val.name }}</n-button>
                 <!-- <InputText v-if="showEditor === index" type="text" v-model="value" size="small" /> -->
                 <div>
                     <n-tag :type="cats[val.name] ? 'warning' : 'error'" size="small" v-if="val.name in cats">
-                        {{ cats[val.name] || 'unsorted' }}
+                        {{ obj[cats[val.name]]?.title || '<error>' }}
                     </n-tag>
                     <n-tag type="info" size="small" v-if="val.name in dict">
                         {{ dict[val.name] }}
@@ -35,6 +35,7 @@ import { useRouter, useRoute } from 'vue-router';
 
 const dict = ref();
 const cats = ref({} as keyable);
+const obj = ref();
 const showEditor = ref();
 const router = useRouter();
 const route = useRoute();
@@ -54,13 +55,15 @@ onBeforeMount(async () => {
     } else {
         console.log("fetching error");
     }
-    const response2 = await fetch('/api/onto');
+    const response2 = await fetch('/api/onto/flat');
     if (response.status === 200) {
         const data = await response2.json();
+        console.log(data);
+        obj.value = data;
         // dict.value = data;
         const dataCats = {} as keyable;
         for (const item in data) {
-            data[item].forEach((x: any) => dataCats[x.name] = item);
+            JSON.parse(data[item]?.names)?.forEach((x: any) => dataCats[x] = data[item].parent);
         }
         cats.value = dataCats;
         isLoaded.value = true;
