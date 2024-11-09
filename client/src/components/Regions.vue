6a364fd1348141b3c1abd1960b0935f8cb1de4e5
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-11-09 19:20:32 +0100

    update URL on map data change

--- client/src/components/Regions.vue
+++ client/src/components/Regions.vue
@@ -5,7 +5,7 @@
             <D3Map :place="id" />
         </div>
         <div>
-            <n-input-number v-model:value="id" clearable :min="1" />
+            <n-input-number @click="showPlace" v-model:value="id" clearable :min="1" />
             <!-- <n-space>
                 <span v-for="index in Array(500).keys()">
                     <n-button :type="route.fullPath !== `/regions/${index + 1}` ? 'default' : 'info'"
@@ -20,10 +20,15 @@
 <script setup lang="ts">
 import store from '../store';
 import D3Map from './D3Map.vue';
-import { ref, onBeforeMount } from 'vue';
-import { useRouter, useRoute } from 'vue-router';
+import { ref, onBeforeMount, toRaw } from 'vue';
+import { useRoute } from 'vue-router';
+import router from '../router';
 
-// const router = useRouter();
-const route = useRoute();
-const id = ref(Number(route.params.id) || 1);
+const vuerouter = useRoute();
+const id = ref(Number(vuerouter.params.id) || 1);
+
+const showPlace = () => {
+    console.log("set place", id.value);
+    router.push(`/regions/${id.value}`)
+}
 </script>
