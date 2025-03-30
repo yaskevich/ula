1448bdf8ad08b33d05866640152937f1d687c0e2
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-03-30 23:31:08 +0200

    create api handler for API ineraction

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,14 +1,22 @@
 import { feature as topofeature } from 'topojson-client';
 import project from '../package.json';
 import topos from '../../data/poland.json';
-import freq from '../../data/freq.json';
 
 const geofeatures = (<any>topofeature((topos as any), (topos.objects.woj as any))).features;
 const git = project?.repository?.url ? 'https' + project.repository.url.slice(3, -4) : '';
 
+const api = async (route: string, params?: keyable) => {
+    const response = await fetch(`/api/${route}?` + new URLSearchParams(params).toString());
+    if (response.status === 200) {
+        return await response.json();
+    } else {
+        console.log("fetching error");
+    }
+}
+
 export default {
     version: project?.version,
     git,
-    freq,
     geofeatures,
+    api,
 };
