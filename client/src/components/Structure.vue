6612a9d4fb64609be29b8c8e8055328db461015e
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-04-06 23:50:43 +0200

    make renderBar universal

--- client/src/components/Structure.vue
+++ client/src/components/Structure.vue
@@ -5,11 +5,12 @@
         style="font-weight:bold;" />
 </template>
 
-
 <script setup lang="ts">
 import { onBeforeMount, onMounted, ref } from 'vue';
 import type { DataTableColumns } from 'naive-ui';
-import * as d3 from 'd3';
+import store from '../store';
+
+const stats = ref<Array<IWord>>();
 
 const rowClassName = (row: IWord) => {
     if (row.qty > 10000) {
@@ -21,7 +22,6 @@ const rowClassName = (row: IWord) => {
     }
     return 'hell';
 };
-const stats = ref<Array<IWord>>();
 
 const rowProps = (row: IWord) => {
     return {
@@ -50,46 +50,12 @@ const getWords = async () => {
     }
 };
 
-const renderBar = () => {
-    if (stats?.value?.length) {
-        const margin = { top: 20, right: 20, bottom: 30, left: 40 },
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-
-        const x = d3.scaleBand().range([0, width]).padding(0.1);
-        const y = d3.scaleLinear().range([height, 0]);
-        const svg = d3.select("#graph").append("svg");
-        svg.attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        const data = stats.value.filter(x => x.words > 6).reverse();
-
-        x.domain(data.map((d) => String(d.words)));
-        y.domain([0, d3.max(data, (d: any) => d.qty)]);
-
-        svg.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", (d) => String(x(String(d.words))))
-            .attr("width", x.bandwidth())
-            .attr("y", (d) => y(d.qty))
-            .attr("height", (d) => height - y(d.qty));
-
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        svg.append("g").call(d3.axisLeft(y));
-    }
-};
-
-
 onMounted(async () => {
     await getWords();
-    renderBar();
+    if (stats.value?.length) {
+        const data = stats.value.filter(x => x.words > 6).reverse();
+        store.renderBar(data as [], 'words', 'qty');
+    }
 });
 </script>
 <style scoped>
--- client/src/store.ts
+++ client/src/store.ts
@@ -1,4 +1,5 @@
 import { reactive } from 'vue';
+import * as d3 from 'd3';
 import { feature as topofeature } from 'topojson-client';
 import project from '../package.json';
 import topos from '../../data/poland.json';
@@ -10,6 +11,46 @@ const git = project?.repository?.url ? 'https' + project.repository.url.slice(3,
 const state = reactive({
     exp: [] as Array<string>,
 });
+
+
+const renderBar = (data: [], keyName: string, valName: string, addKey = false) => {
+    if (data?.length) {
+        console.log(data);
+
+        const margin = { top: 20, right: 20, bottom: 30, left: 40 },
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        const x = d3.scaleBand().range([0, width]).padding(0.1);
+        const y = d3.scaleLinear().range([height, 0]);
+        const svg = d3.select("#graph").append("svg");
+        svg.attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+        const formatKey = (x: any) => String(x[keyName]) + (addKey ? ` (${String(x[valName])})` : '');
+
+        x.domain(data.map((d) => formatKey(d)));
+        y.domain([0, d3.max(data, (d: any) => d[valName])]);
+
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", (d) => String(x(String(formatKey(d)))))
+            .attr("width", x.bandwidth())
+            .attr("y", (d) => y(d[valName]))
+            .attr("height", (d) => height - y(d[valName]));
+
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        svg.append("g").call(d3.axisLeft(y));
+    }
+};
 
 const api = async (route: string, params?: keyable) => {
     const response = await fetch(`/api/${route}?` + new URLSearchParams(params).toString());
@@ -26,4 +67,5 @@ export default {
     geofeatures,
     api,
     state,
+    renderBar,
 };
